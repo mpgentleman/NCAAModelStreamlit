7740803a0f4741bdd1c4808bf9bb87899b5148ce
mpgentleman <80001401+mpgentleman@users.noreply.github.com>
2021-03-04 19:50:43 -0600

    Update NCAAGameStream.py

--- NCAAGameStream.py
+++ NCAAGameStream.py
@@ -170,137 +170,137 @@ dateforRankings5=d2
 #dateforRankings5=datesUnderlineAdd[0]
 
 
+if st.button('Run'):
+
+    TeamDatabase2=pd.read_csv("Data/TeamDatabase.csv")
+    TeamDatabase2.set_index("OldTRankName", inplace=True)
+    MG_DF1=pd.read_csv("Data/MGRankings/tm_seasons_stats_ranks"+dateforRankings5+".csv")
+    MG_DF1["updated"]=update_type(MG_DF1.tm,TeamDatabase2.UpdatedTRankName)
+    MG_DF1.set_index("updated", inplace=True)
+    from matplotlib.backends.backend_pdf import PdfPages
+    WhichFile='TeamDataFiles2021'
+    pp= PdfPages("Daily_Team_Charts_"+dateToday+".pdf")
+    Dailyschedule=pd.read_csv("Data/DailySchedules2021/"+dateToday+"Schedule.csv")
+    Dailyschedule=Dailyschedule.sort_values(by=['Reg_dif'])
+    st.header('Games Today')
+
+    import plotly.graph_objects as go
+    import pandas as pd
+    lengthrows=int(len(Dailyschedule)/2)
+    rowEvenColor = 'lightgrey'
+    rowOddColor = 'white'
+    fig = go.Figure(data=[go.Table(
+        header=dict(values=list(Dailyschedule.columns),
+                    fill_color=rowEvenColor,
+                    align='left'),
+        cells=dict(values=[Dailyschedule.AWAY, Dailyschedule.HOME, Dailyschedule.VegasSpread, Dailyschedule.VegasTotal, Dailyschedule.Court, Dailyschedule.Time,Dailyschedule.Reg_dif],
+        fill_color = [[rowOddColor,rowEvenColor]*lengthrows],
+                align='left',
+        font_size=12,
+        height=30))
+    ])
 
 
-TeamDatabase2=pd.read_csv("Data/TeamDatabase.csv")
-TeamDatabase2.set_index("OldTRankName", inplace=True)
-MG_DF1=pd.read_csv("Data/MGRankings/tm_seasons_stats_ranks"+dateforRankings5+".csv")
-MG_DF1["updated"]=update_type(MG_DF1.tm,TeamDatabase2.UpdatedTRankName)
-MG_DF1.set_index("updated", inplace=True)
-from matplotlib.backends.backend_pdf import PdfPages
-WhichFile='TeamDataFiles2021'
-pp= PdfPages("Daily_Team_Charts_"+dateToday+".pdf")
-Dailyschedule=pd.read_csv("Data/DailySchedules2021/"+dateToday+"Schedule.csv")
-Dailyschedule=Dailyschedule.sort_values(by=['Reg_dif'])
-st.header('Games Today')
-
-import plotly.graph_objects as go
-import pandas as pd
-lengthrows=int(len(Dailyschedule)/2)
-rowEvenColor = 'lightgrey'
-rowOddColor = 'white'
-fig = go.Figure(data=[go.Table(
-    header=dict(values=list(Dailyschedule.columns),
-                fill_color=rowEvenColor,
-                align='left'),
-    cells=dict(values=[Dailyschedule.AWAY, Dailyschedule.HOME, Dailyschedule.VegasSpread, Dailyschedule.VegasTotal, Dailyschedule.Court, Dailyschedule.Time,Dailyschedule.Reg_dif],
-    fill_color = [[rowOddColor,rowEvenColor]*lengthrows],
-               align='left',
-    font_size=12,
-    height=30))
-])
-
-
-fig.update_layout(width=1200, height=800)
+    fig.update_layout(width=1200, height=800)
 #fig.show()
-st.plotly_chart(fig)
-AwayList=list(Dailyschedule['AWAY'])
-HomeList=list(Dailyschedule['HOME'])
+    st.plotly_chart(fig)
+    AwayList=list(Dailyschedule['AWAY'])
+    HomeList=list(Dailyschedule['HOME'])
 
-AwayTeam = st.sidebar.selectbox('Away Team',AwayList)
-HomeTeam = st.sidebar.selectbox('Home Team',HomeList)
-whereIsGame = st.sidebar.selectbox('Neutral Site',['Yes', 'No'])
+    AwayTeam = st.sidebar.selectbox('Away Team',AwayList)
+    HomeTeam = st.sidebar.selectbox('Home Team',HomeList)
+    whereIsGame = st.sidebar.selectbox('Neutral Site',['Yes', 'No'])
 
 
-TeamDatabase=pd.read_csv("Data/TeamDatabase.csv")
-#TeamDatabase.set_index("University", inplace=True)
-#TeamList=list(TeamDatabase1["OldTRankName"])
-TeamDatabase.set_index("OldTRankName", inplace=True)
-Dailyschedule['VegasSpread'] = Dailyschedule.VegasSpread.apply(calculate_to_numeric)
-Dailyschedule['Total'] = Dailyschedule.VegasTotal.apply(calculate_to_numeric)   
-#PomeroyDF1=GetPomeroyData()
-PomeroyDF1=pd.read_csv("Data/PomeroyDailyRankings2021/PomeroyRankings"+dateforRankings+".csv")
-#PomeroyDF1=sanitizeEntireColumn(PomeroyDF1,"Team")
-PomeroyDF1["updated"]=update_type(PomeroyDF1.Team,TeamDatabase.set_index('PomeroyName').UpdatedTRankName)
-PomeroyDF1["updated"]=PomeroyDF1["updated"].str.rstrip()
+    TeamDatabase=pd.read_csv("Data/TeamDatabase.csv")
+    #TeamDatabase.set_index("University", inplace=True)
+    #TeamList=list(TeamDatabase1["OldTRankName"])
+    TeamDatabase.set_index("OldTRankName", inplace=True)
+    Dailyschedule['VegasSpread'] = Dailyschedule.VegasSpread.apply(calculate_to_numeric)
+    Dailyschedule['Total'] = Dailyschedule.VegasTotal.apply(calculate_to_numeric)   
+    #PomeroyDF1=GetPomeroyData()
+    PomeroyDF1=pd.read_csv("Data/PomeroyDailyRankings2021/PomeroyRankings"+dateforRankings+".csv")
+    #PomeroyDF1=sanitizeEntireColumn(PomeroyDF1,"Team")
+    PomeroyDF1["updated"]=update_type(PomeroyDF1.Team,TeamDatabase.set_index('PomeroyName').UpdatedTRankName)
+    PomeroyDF1["updated"]=PomeroyDF1["updated"].str.rstrip()
    
-PomeroyDF1.set_index("updated", inplace=True)
+    PomeroyDF1.set_index("updated", inplace=True)
 
-BartDF1=pd.read_csv("Data/TRankDailyRankings2021/"+dateforRankings+".csv")
-#getBartDataTest()
-#print(BartDF1)  
-BartDF1["updated"]=update_type(BartDF1.Team,TeamDatabase.set_index('TRankName').UpdatedTRankName)
+    BartDF1=pd.read_csv("Data/TRankDailyRankings2021/"+dateforRankings+".csv")
+    #getBartDataTest()
+    #print(BartDF1)  
+    BartDF1["updated"]=update_type(BartDF1.Team,TeamDatabase.set_index('TRankName').UpdatedTRankName)
 
-BartDF1.set_index("updated", inplace=True)
-
-
-import seaborn
-MonteCarloNumberofGames=5
-SecondMonteCarloNumberofGames=10
-LeagueTempo=70.2
-LeagueOE=102.2
-LeagueOE=102.9
-OverplayingList=[]
-OverplayingList2=[]
-appendTeamList=[]
-appended_data1=[]
-appended_dataExtra=[]
-appended_dataTime=[]
-appendTeam_OU_List=[]
-
-appended_data1MG=[]
-appendTeamListMG=[]
+    BartDF1.set_index("updated", inplace=True)
 
 
+    import seaborn
+    MonteCarloNumberofGames=5
+    SecondMonteCarloNumberofGames=10
+    LeagueTempo=70.2
+    LeagueOE=102.2
+    LeagueOE=102.9
+    OverplayingList=[]
+    OverplayingList2=[]
+    appendTeamList=[]
+    appended_data1=[]
+    appended_dataExtra=[]
+    appended_dataTime=[]
+    appendTeam_OU_List=[]
 
-st.title('NCAA Game Matchup')
-
-#### Sidebar Creation #######
+    appended_data1MG=[]
+    appendTeamListMG=[]
 
 
 
-test1=GetThisTeamInfoFromCsv(AwayTeam,"TeamDataFiles2021")
+    st.title('NCAA Game Matchup')
 
-test2=GetThisTeamInfoFromCsv(HomeTeam,"TeamDataFiles2021")
-AwayTeamB=TeamDatabase.loc[AwayTeam,"UpdatedTRankName"]
-HomeTeamB=TeamDatabase.loc[HomeTeam,"UpdatedTRankName"]
+    #### Sidebar Creation #######
 
-AwayTeamP=TeamDatabase.loc[AwayTeam,"UpdatedTRankName"]
-HomeTeamP=TeamDatabase.loc[HomeTeam,"UpdatedTRankName"]
+
+
+    test1=GetThisTeamInfoFromCsv(AwayTeam,"TeamDataFiles2021")
+
+    test2=GetThisTeamInfoFromCsv(HomeTeam,"TeamDataFiles2021")
+    AwayTeamB=TeamDatabase.loc[AwayTeam,"UpdatedTRankName"]
+    HomeTeamB=TeamDatabase.loc[HomeTeam,"UpdatedTRankName"]
+
+    AwayTeamP=TeamDatabase.loc[AwayTeam,"UpdatedTRankName"]
+    HomeTeamP=TeamDatabase.loc[HomeTeam,"UpdatedTRankName"]
     
-AwayTeamM=TeamDatabase2.loc[AwayTeam,"SportsReference"]
-HomeTeamM=TeamDatabase2.loc[HomeTeam,"SportsReference"]
-test1['AdjEM_MG']=test1['AdjOE_MG']-test1['AdjDE_MG']
-test2['AdjEM_MG']=test2['AdjOE_MG']-test2['AdjDE_MG']
+    AwayTeamM=TeamDatabase2.loc[AwayTeam,"SportsReference"]
+    HomeTeamM=TeamDatabase2.loc[HomeTeam,"SportsReference"]
+    test1['AdjEM_MG']=test1['AdjOE_MG']-test1['AdjDE_MG']
+    test2['AdjEM_MG']=test2['AdjOE_MG']-test2['AdjDE_MG']
 
-#NF.GetTwoChartsTogether_EMA(test1,test2,AwayTeam,HomeTeam,"EMRating","EMRating","PomAdjEMCurrent","PomAdjEMCurrent","ATS")
-plt.style.use('seaborn')
+    #NF.GetTwoChartsTogether_EMA(test1,test2,AwayTeam,HomeTeam,"EMRating","EMRating","PomAdjEMCurrent","PomAdjEMCurrent","ATS")
+    plt.style.use('seaborn')
     
-test1['New_ID'] = range(0, 0+len(test1))
-test2['New_ID'] = range(0, 0+len(test2))
-#p=sns.regplot(x="New_ID", y="EMRating", data=DftoChange,order=2);
-fig_dims = (15,10)
-fig, (ax1, ax2) = plt.subplots(ncols=2, sharey=True,figsize=fig_dims)
+    test1['New_ID'] = range(0, 0+len(test1))
+    test2['New_ID'] = range(0, 0+len(test2))
+    #p=sns.regplot(x="New_ID", y="EMRating", data=DftoChange,order=2);
+    fig_dims = (15,10)
+    fig, (ax1, ax2) = plt.subplots(ncols=2, sharey=True,figsize=fig_dims)
     #fig, axs = plt.subplots(ncols=2,figsize=fig_dims)
-plt.figure(figsize=(20, 12))
-ax1.set_title(AwayTeam)
-ax2.set_title(HomeTeam)
-fig1=sns.regplot(x="New_ID", y="EMRating5GameExpMA", data=test1,order=2, ax=ax1)
-fig2=sns.regplot(x='New_ID', y='PomAdjEMCurrent', data=test1,order=2, ax=ax1)
-fig3=sns.regplot(x="New_ID", y="EMRating5GameExpMA", data=test2,order=2, ax=ax2)
-fig4=sns.regplot(x='New_ID', y='PomAdjEMCurrent', data=test2,order=2, ax=ax2)   
-st.pyplot(fig)
+    plt.figure(figsize=(20, 12))
+    ax1.set_title(AwayTeam)
+    ax2.set_title(HomeTeam)
+    fig1=sns.regplot(x="New_ID", y="EMRating5GameExpMA", data=test1,order=2, ax=ax1)
+    fig2=sns.regplot(x='New_ID', y='PomAdjEMCurrent', data=test1,order=2, ax=ax1)
+    fig3=sns.regplot(x="New_ID", y="EMRating5GameExpMA", data=test2,order=2, ax=ax2)
+    fig4=sns.regplot(x='New_ID', y='PomAdjEMCurrent', data=test2,order=2, ax=ax2)   
+    st.pyplot(fig)
 
-GetTwoChartsTogether_EMA(test1,test2,AwayTeam,HomeTeam,"EMRating","EMRating","PomAdjEMCurrent","PomAdjEMCurrent","ATS")
-#GetTwoChartsTogether_EMA(test1,test2,AwayTeam,HomeTeam,"EMRating","EMRating",'AdjEM_MG','AdjEM_MG',"ATS")
-GetTwoTeamChartsTogetherDec6(pp,test1,test2,AwayTeam,HomeTeam,"EMRating","PomAdjEMCurrent","ATS")
-getOverplayingChartBothTeamsDec4(pp,test1,test2,AwayTeam,HomeTeam)
-GetTwoTeamChartsTogetherDec6(pp,test1,test2,AwayTeam,HomeTeam,"AdjO","PomAdjOECurrent","ATS")
-GetTwoTeamChartsTogetherDec6(pp,test1,test2,AwayTeam,HomeTeam,"AdjD","PomAdjDECurrent","OverUnder")
-GetTwoTeamChartsTogetherDec6(pp,test1,test2,AwayTeam,HomeTeam,"Pace","PomTempo","OverUnder")
+    GetTwoChartsTogether_EMA(test1,test2,AwayTeam,HomeTeam,"EMRating","EMRating","PomAdjEMCurrent","PomAdjEMCurrent","ATS")
+    #GetTwoChartsTogether_EMA(test1,test2,AwayTeam,HomeTeam,"EMRating","EMRating",'AdjEM_MG','AdjEM_MG',"ATS")
+    GetTwoTeamChartsTogetherDec6(pp,test1,test2,AwayTeam,HomeTeam,"EMRating","PomAdjEMCurrent","ATS")
+    getOverplayingChartBothTeamsDec4(pp,test1,test2,AwayTeam,HomeTeam)
+    GetTwoTeamChartsTogetherDec6(pp,test1,test2,AwayTeam,HomeTeam,"AdjO","PomAdjOECurrent","ATS")
+    GetTwoTeamChartsTogetherDec6(pp,test1,test2,AwayTeam,HomeTeam,"AdjD","PomAdjDECurrent","OverUnder")
+    GetTwoTeamChartsTogetherDec6(pp,test1,test2,AwayTeam,HomeTeam,"Pace","PomTempo","OverUnder")
   
-GetTwoTeamChartsTogether(test1,test2,AwayTeam,HomeTeam,"PPP","ATS")
-GetTwoTeamChartsTogether(test1,test2,AwayTeam,HomeTeam,"PPP1","OverUnder")
+    GetTwoTeamChartsTogether(test1,test2,AwayTeam,HomeTeam,"PPP","ATS")
+    GetTwoTeamChartsTogether(test1,test2,AwayTeam,HomeTeam,"PPP1","OverUnder")
 
 #st.pyplot(fig2)
 

8e3449af12515ca85d641f85a0d13a0142d4e8ee
mpgentleman <80001401+mpgentleman@users.noreply.github.com>
2023-12-28 19:23:14 -0600

    Update NCAAGameStream.py

--- NCAAGameStream.py
+++ NCAAGameStream.py
@@ -2331,8 +2331,9 @@ def Bracketology_Page(data):
     teamsdict['all'] = all_teams
     regional_rankings = regional_rankings 
     SimulationResults = namedtuple('SimulationResults','brackets unique_brackets lowest_bracket lowest_bracket_count most_common_bracket most_common_bracket_count')
-    set_energy_function = set_energy_function
+    
     set_energy_function(default_energy_game)
+    set_energy_function = set_energy_function
     #set_energy_function(My_energy_game)
     kenpom = {}
 

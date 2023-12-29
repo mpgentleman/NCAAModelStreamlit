1c0d5f1bd0d0041bd0ba1038fa3b4628cacb69b2
mpgentleman <80001401+mpgentleman@users.noreply.github.com>
2023-12-28 21:32:25 -0600

    Update NCAAGameStream.py

--- NCAAGameStream.py
+++ NCAAGameStream.py
@@ -2575,7 +2575,7 @@ def Bracketology_Page(data):
         else:
             myranks = PomDict
         
-'
+
     results = runbracket1(ntrials=20000,T=.1,myranks)
     j=maketabletest(results)
     allrounds = ['1st Round','2nd Round','3rd Round','Sweet 16','Elite 8','Final 4','Championship','Win']

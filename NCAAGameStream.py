e9b4706a7385c6b29fa67c47f51b2ff42370aacf
mpgentleman <80001401+mpgentleman@users.noreply.github.com>
2024-01-10 20:55:34 -0600

    Update NCAAGameStream.py

--- NCAAGameStream.py
+++ NCAAGameStream.py
@@ -2591,8 +2591,12 @@ def displayTeamDistributionsMatchup(Gamesdf,myteam,team2):
 
     density11  = ggplot(dff1, aes(x='AdjO', color='Team')) + ggsize(800, 550)+ geom_density(aes(fill='Team'), alpha=.3) + scale_fill_brewer(type='seq')+ ggtitle("Adjusted Offensive Rating")
     density21  = ggplot(dff1, aes(x='AdjD', color='Team')) + ggsize(800, 550)+ geom_density(aes(fill='Team'), alpha=.3)+ scale_fill_brewer(type='seq')+ ggtitle("Adjusted Defensive Rating")
-    density31  = ggplot(dff1, aes(x='EMRating', color='Team')) + ggsize(800, 550)+ geom_density(aes(fill='Team'), alpha=.3)+ scale_fill_brewer(type='seq')+ ggtitle("Adjusted Efficiency Rating")+ geom_vline(xintercept=df1['EMRating'].median())+ geom_vline(xintercept=df['EMRating'].median(), color="red")
-    density41 = ggplot(dff1, aes(x='Tempo', color='Team')) + ggsize(800, 550)+ geom_density(aes(fill='Team'), alpha=.3)+ scale_fill_brewer(type='seq')+ ggtitle("Pace/Tempo")+ geom_vline(xintercept=df1['Tempo'].median())+ geom_vline(xintercept=df['Tempo'].median(), color="red")
+    density31  = ggplot(dff1, aes(x='EMRating', color='Team')) + ggsize(800, 550)+ geom_density(aes(fill='Team'), alpha=.3)+ scale_fill_brewer(type='seq')+ ggtitle("Adjusted Efficiency Rating")+ geom_vline(xintercept=df1['EMRating'].median())+ geom_vline(xintercept=df['EMRating'].median(), color="red")+ \
+    geom_text(aes(label='y'), position=position_nudge(y=1), \
+              label_format='.1f', angle=45, color='#2b8cbe')
+    density41 = ggplot(dff1, aes(x='Tempo', color='Team')) + ggsize(800, 550)+ geom_density(aes(fill='Team'), alpha=.3)+ scale_fill_brewer(type='seq')+ ggtitle("Pace/Tempo")+ geom_vline(xintercept=df1['Tempo'].median())+ geom_vline(xintercept=df['Tempo'].median(), color="red")+ \
+    geom_text(aes(label='y'), position=position_nudge(y=1), \
+              label_format='.1f', angle=45, color='#2b8cbe')
     p2 = gggrid([density1,density2,density3,density4,density12,density22,density32,density42,density11,density21,density31,density41], ncol=4)+ ggsize(2000, 1000)
     st.subheader(' Distribution Charts')
     #st_letsplot(p2)

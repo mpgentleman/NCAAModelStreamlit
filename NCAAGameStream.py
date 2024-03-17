e4787872bba24de89a5272f11350b17489ef3d75
mpgentleman <80001401+mpgentleman@users.noreply.github.com>
2024-03-17 17:54:29 -0500

    Update NCAAGameStream.py

--- NCAAGameStream.py
+++ NCAAGameStream.py
@@ -3977,6 +3977,8 @@ BM = getBracketMatrixDataframe()
 TBracket = getTRankBracket()
 TBracket1 = TBracket[['Seed','east','midwest','south','west']]
 BM1 = BM[['Seed','east','midwest','south','west']]
+st.dataframe(BM1)
+st.dataframe(TBracket1)
 TBracket1 = pd.read_csv("Data/Tourney2024.csv")
 newsouth=list(TBracket1["south"])
 neweast=list(TBracket1["east"])

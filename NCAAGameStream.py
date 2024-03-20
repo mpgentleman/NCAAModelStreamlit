b580988356f063cdeb1c8a3bac7b94c9c6553298
mpgentleman <80001401+mpgentleman@users.noreply.github.com>
2024-03-19 20:03:09 -0500

    Update NCAAGameStream.py

--- NCAAGameStream.py
+++ NCAAGameStream.py
@@ -3034,7 +3034,7 @@ def MG_RankingsRecent(data):
     st.write('ATS_PREMIUM highlights that premium or deficit. It should correspond to teams ATS record')
     dateString=str(add_selectbox_start)
     dateToday=dateString.replace('-', '')
-    files = os.listdir('Data/MGRankings2024Recent')
+    files = os.listdir('Data/MGrankings2024Recent')
 
     # Filter the list to include only files that start with 'MG'
     files = [file for file in files if file.startswith('MG')]
@@ -3053,7 +3053,7 @@ def MG_RankingsRecent(data):
         st.write(latest_date)
         myfilestring = file_dict[latest_date]
     #st.write(myfilestring)
-    myfile = "Data/MGRankings2024Recent/"+myfilestring
+    myfile = "Data/MGrankings2024Recent/"+myfilestring
     HtmlFile = open(myfile, 'r', encoding='utf-8')
     source_code = HtmlFile.read() 
     #print(source_code)

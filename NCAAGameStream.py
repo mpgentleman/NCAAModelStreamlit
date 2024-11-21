57a899955b4cdaba143730848ac1a9a70c67a5e4
mpgentleman <80001401+mpgentleman@users.noreply.github.com>
2024-11-21 07:33:21 -0600

    Update NCAAGameStream.py

--- NCAAGameStream.py
+++ NCAAGameStream.py
@@ -83,7 +83,7 @@ def showYesterdaysChart(Betting):
     #scatter_data = Betting[Betting['Date'].isin(mylist)]
     #scatter_data = Betting[Betting['Date_zero']==new_date_str]
     #new_date_str
-    scatter_data = scatter_data.dropna(subset=["MG_ATS_PointDiff", "ATSVegas"])
+    scatter_data = Betting.dropna(subset=["MG_ATS_PointDiff", "ATSVegas"])
     scatter_data["MG_ATS_PointDiff"] = pd.to_numeric(scatter_data["MG_ATS_PointDiff"], errors='coerce')
     scatter_data["ATSVegas"] = pd.to_numeric(scatter_data["ATSVegas"], errors='coerce')
     scatter_data["SpreadDif"] = scatter_data["MG_ATS_PointDiff"]-scatter_data["ATSVegas"]
